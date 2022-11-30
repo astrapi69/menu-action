f1ba5971c118508da320021b54c4f7bf8864847c
asterios.raptis <asterios.raptis@gmx.net>
2022-11-30 16:44:44 +0100

    feat: created new factory methods for create JPopupMenu with MenuInfo objects
    - new unit tests for JMenuBarFactory created
    - removed unit test from MenuInfoTest

--- src/main/java/io/github/astrapi69/swing/menu/factory/JPopupMenuFactory.java
+++ src/main/java/io/github/astrapi69/swing/menu/factory/JPopupMenuFactory.java
@@ -24,17 +24,14 @@
  */
 package io.github.astrapi69.swing.menu.factory;
 
-import io.github.astrapi69.swing.menu.model.MenuItemInfo;
-import io.github.astrapi69.swing.menu.model.PopupMenuInfo;
-import io.github.astrapi69.swing.menu.popup.listener.PopupListener;
-
-import javax.swing.JMenuItem;
-import javax.swing.JPopupMenu;
-import java.awt.Component;
-import java.awt.MenuItem;
-import java.awt.PopupMenu;
+import java.awt.*;
 import java.awt.event.MouseListener;
 import java.util.List;
+
+import javax.swing.*;
+
+import io.github.astrapi69.swing.menu.model.MenuItemInfo;
+import io.github.astrapi69.swing.menu.popup.listener.PopupListener;
 
 /**
  * A factory {@link JPopupMenuFactory} provides factory methods for create JPopupMenu objects
@@ -82,9 +79,8 @@ public class JPopupMenuFactory
 	}
 
 	/**
-	 * Factory method for create a <code>JPopupMenu</code> that will be add a
-	 * <code>MouseListener</code> to the given <code>Component</code> and an array of
-	 * <code>JMenuItem</code> that will be added to the popup.
+	 * Factory method for create a new {@link JPopupMenu} object that will be added to the given
+	 * <code>Component</code> and an array of <code>JMenuItem</code> that will be added to the popup
 	 *
 	 * @param label
 	 *            the label
@@ -97,11 +93,57 @@ public class JPopupMenuFactory
 	public static JPopupMenu newJPopupMenu(final String label, final Component component,
 		final JMenuItem... items)
 	{
+		return newJPopupMenu(component, label, items);
+	}
+
+	/**
+	 * Factory method for create a new {@link JPopupMenu} object that will be added to the given
+	 * <code>Component</code> and an array of <code>JMenuItem</code> that will be added to the popup
+	 *
+	 * @param component
+	 *            the component to add the <code>JPopupMenu</code>
+	 * @param label
+	 *            the label from the <code>JPopupMenu</code>
+	 * @param items
+	 *            the <code>JMenuItem</code>s
+	 * @return the new {@link JPopupMenu} object
+	 */
+	public static JPopupMenu newJPopupMenu(final Component component, final String label,
+		final JMenuItem... items)
+	{
 		// Create the popup menu.
 		final JPopupMenu popup = newJPopupMenu(label);
 		for (final JMenuItem menuItem : items)
 		{
 			popup.add(menuItem);
+		}
+		// Add listener to the component so the popup menu can come up.
+		final MouseListener popupListener = new PopupListener(popup);
+		component.addMouseListener(popupListener);
+		return popup;
+	}
+
+	/**
+	 * Factory method for create a new {@link JPopupMenu} object that will be added to the given
+	 * <code>Component</code> and an array of <code>JMenuItem</code> that will be added to the
+	 * popup.
+	 *
+	 * @param component
+	 *            the component to add the <code>JPopupMenu</code>
+	 * @param label
+	 *            the label from the <code>JPopupMenu</code>
+	 * @param items
+	 *            the <code>JMenuItem</code>s
+	 * @return the new {@link JPopupMenu} object
+	 */
+	public static JPopupMenu newJPopupMenu(final Component component, final String label,
+		final MenuItemInfo... items)
+	{
+		// Create the popup menu.
+		final JPopupMenu popup = newJPopupMenu(label);
+		for (final MenuItemInfo menuItem : items)
+		{
+			popup.add(menuItem.toJMenuItem());
 		}
 		// Add listener to the component so the popup menu can come up.
 		final MouseListener popupListener = new PopupListener(popup);
--- src/test/java/io/github/astrapi69/swing/menu/factory/JMenuBarFactoryTest.java
+++ src/test/java/io/github/astrapi69/swing/menu/factory/JMenuBarFactoryTest.java
@@ -24,23 +24,97 @@
  */
 package io.github.astrapi69.swing.menu.factory;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.*;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
 import io.github.astrapi69.file.create.FileFactory;
 import io.github.astrapi69.file.read.ReadFileExtensions;
 import io.github.astrapi69.file.search.PathFinder;
+import io.github.astrapi69.id.generate.LongIdGenerator;
+import io.github.astrapi69.junit.jupiter.callback.before.test.IgnoreHeadlessExceptionExtension;
+import io.github.astrapi69.swing.action.ExitApplicationAction;
+import io.github.astrapi69.swing.action.NoAction;
+import io.github.astrapi69.swing.action.ToggleFullScreenAction;
+import io.github.astrapi69.swing.menu.MenuExtensions;
+import io.github.astrapi69.swing.menu.enumtype.BaseMenuId;
+import io.github.astrapi69.swing.menu.enumtype.MenuType;
+import io.github.astrapi69.swing.menu.model.KeyStrokeInfo;
 import io.github.astrapi69.swing.menu.model.MenuInfo;
 import io.github.astrapi69.tree.BaseTreeNode;
-import org.junit.jupiter.api.Test;
-
-import java.io.File;
-import java.io.IOException;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 public class JMenuBarFactoryTest
 {
 	@Test
 	public void testBuildRootTreeNodeWithXml() throws IOException
+	{
+		BaseTreeNode<MenuInfo, Long> menuBarTreeNode;
+		BaseTreeNode<MenuInfo, Long> fileTreeNode;
+		BaseTreeNode<MenuInfo, Long> toggleFullscreenTreeNode;
+		BaseTreeNode<MenuInfo, Long> exitTreeNode;
+		MenuInfo menuBarInfo;
+		MenuInfo fileMenuInfo;
+		MenuInfo toggleFullscreenMenuInfo;
+		MenuInfo exitMenuInfo;
+		LongIdGenerator idGenerator;
+		File xmlFile;
+		String xml;
+		BaseTreeNode<MenuInfo, Long> menuInfoLongBaseTreeNode;
+
+		idGenerator = LongIdGenerator.of(0L);
+
+		menuBarInfo = MenuInfo.builder().type(MenuType.MENU_BAR)
+			.name(BaseMenuId.MENU_BAR.propertiesKey()).build();
+
+		menuBarTreeNode = BaseTreeNode.<MenuInfo, Long> builder().id(idGenerator.getNextId())
+			.value(menuBarInfo).build();
+
+		fileMenuInfo = MenuInfo.builder().mnemonic(MenuExtensions.toMnemonic('F'))
+			.keyStrokeInfo(KeyStrokeInfo.toKeyStrokeInfo(KeyStroke.getKeyStroke("alt pressed F")))
+			.text("File").name(BaseMenuId.FILE.propertiesKey()).build();
+		fileTreeNode = BaseTreeNode.<MenuInfo, Long> builder().id(idGenerator.getNextId())
+			.value(fileMenuInfo).build();
+
+		toggleFullscreenMenuInfo = MenuInfo.builder().type(MenuType.MENU_ITEM)
+			.mnemonic(MenuExtensions.toMnemonic('T'))
+			.keyStrokeInfo(KeyStrokeInfo.toKeyStrokeInfo(KeyStroke.getKeyStroke("alt pressed F11")))
+			.text("Toggle Fullscreen").name(BaseMenuId.TOGGLE_FULLSCREEN.propertiesKey()).build();
+		toggleFullscreenTreeNode = BaseTreeNode.<MenuInfo, Long> builder()
+			.id(idGenerator.getNextId()).parent(fileTreeNode).value(toggleFullscreenMenuInfo)
+			.leaf(true).build();
+
+		exitMenuInfo = MenuInfo.builder().type(MenuType.MENU_ITEM)
+			.mnemonic(MenuExtensions.toMnemonic('E'))
+			.keyStrokeInfo(KeyStrokeInfo.toKeyStrokeInfo(KeyStroke.getKeyStroke("alt pressed F4")))
+			.text("Exit").name(BaseMenuId.EXIT.propertiesKey()).build();
+		exitTreeNode = BaseTreeNode.<MenuInfo, Long> builder().id(idGenerator.getNextId())
+			.leaf(true).parent(fileTreeNode).value(exitMenuInfo).build();
+
+		menuBarTreeNode.addChild(fileTreeNode);
+		fileTreeNode.addChild(toggleFullscreenTreeNode);
+		fileTreeNode.addChild(exitTreeNode);
+
+		xmlFile = FileFactory.newFileQuietly(PathFinder.getSrcTestResourcesDir(), "app-menu.xml");
+		xml = ReadFileExtensions.fromFile(xmlFile);
+		menuInfoLongBaseTreeNode = JMenuBarFactory.buildRootTreeNode(xml);
+		assertNotNull(menuInfoLongBaseTreeNode);
+		assertEquals(menuInfoLongBaseTreeNode, menuBarTreeNode);
+
+	}
+
+	@ExtendWith(IgnoreHeadlessExceptionExtension.class)
+	@Test
+	public void testBuildMenuBarWithXml() throws IOException
 	{
 		final File srcTestResourcesDir = PathFinder.getSrcTestResourcesDir();
 		File xmlFile = FileFactory.newFileQuietly(srcTestResourcesDir, "app-menu.xml");
@@ -49,5 +123,17 @@ public class JMenuBarFactoryTest
 			.buildRootTreeNode(xml);
 		assertNotNull(menuInfoLongBaseTreeNode);
 		assertEquals(menuInfoLongBaseTreeNode.getId(), 0);
+
+		final Map<String, ActionListener> actionListenerMap = new HashMap<>();
+
+		actionListenerMap.put(BaseMenuId.TOGGLE_FULLSCREEN.propertiesKey(),
+			new ToggleFullScreenAction("Fullscreen", new JFrame()));
+		actionListenerMap.put(BaseMenuId.EXIT.propertiesKey(), new ExitApplicationAction("Exit"));
+		actionListenerMap.put(BaseMenuId.FILE.propertiesKey(), new NoAction());
+		actionListenerMap.put(BaseMenuId.MENU_BAR.propertiesKey(), new NoAction());
+
+		final JMenuBar menuBar = JMenuBarFactory.buildMenuBar(menuInfoLongBaseTreeNode,
+			actionListenerMap);
+		assertNotNull(menuBar);
 	}
 }
