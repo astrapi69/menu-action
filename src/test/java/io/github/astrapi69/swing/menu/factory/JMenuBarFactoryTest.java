9a9bef7e7cd1b0208bd9c9733bd4c77e12e0fd90
astrapi69 <asterios.raptis@gmx.net>
2022-12-12 17:54:44 +0100

    prepare new minor release in version 2.4
    - junit fixed

--- src/test/java/io/github/astrapi69/swing/menu/factory/JMenuBarFactoryTest.java
+++ src/test/java/io/github/astrapi69/swing/menu/factory/JMenuBarFactoryTest.java
@@ -64,7 +64,7 @@ import io.github.astrapi69.tree.BaseTreeNode;
 public class JMenuBarFactoryTest
 {
 
-
+	@ExtendWith(IgnoreHeadlessExceptionExtension.class)
 	@Test
 	public void testBuildXmlFromMenubar() throws IOException
 	{
